ad0f55480ea02195909ebdd5aed88403795056ea
54678565do <ezeddossa@gmail.com>
2024-09-15 01:51:18 +0100

    nouvelle mise à jour

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         
-        .form-group {
+        .form-group { 
             position: relative;
         }
         .form-group label {
